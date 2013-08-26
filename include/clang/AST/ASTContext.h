362af383d719a9ca8c4be2659fb3007a2739219d
Axel Naumann <Axel.Naumann@cern.ch>
2013-08-26 14:44:44 +0200

    Default template parameter of class template instance keep partially desugared types (i.e. Double32_t)

--- include/clang/AST/ASTContext.h
+++ include/clang/AST/ASTContext.h
@@ -1222,7 +1222,8 @@ public:
                              QualType equivalentType);
 
   QualType getSubstTemplateTypeParmType(const TemplateTypeParmType *Replaced,
-                                        QualType Replacement) const;
+                                        QualType Replacement,
+                                        bool AllowNonCanonical = false) const;
   QualType getSubstTemplateTypeParmPackType(
                                           const TemplateTypeParmType *Replaced,
                                             const TemplateArgument &ArgPack);
