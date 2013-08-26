4abbaf5ba028fe19f8b135dfe22fea2475a5136a
Axel Naumann <Axel.Naumann@cern.ch>
2013-08-26 14:31:39 +0200

    Do not reset TUScope for incremental processing.

--- lib/Sema/Sema.cpp
+++ lib/Sema/Sema.cpp
@@ -649,7 +649,8 @@ void Sema::ActOnEndOfTranslationUnit() {
 
   if (TUKind == TU_Prefix) {
     // Translation unit prefixes don't need any of the checking below.
-    TUScope = 0;
+    if (!PP.isIncrementalProcessingEnabled())
+       TUScope = 0;
     return;
   }
 
@@ -832,7 +833,8 @@ void Sema::ActOnEndOfTranslationUnit() {
   assert(ParsingInitForAutoVars.empty() &&
          "Didn't unmark var as having its initializer parsed");
 
-  TUScope = 0;
+  if (!PP.isIncrementalProcessingEnabled())
+     TUScope = 0;
 }
 
 
