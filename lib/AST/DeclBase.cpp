c51ccf5b5591339429596b2fb9a8a84e4b13f39c
Vassil Vassilev <vvasilev@cern.ch>
2013-10-10 18:12:07 +0200

    Don't try to remove decls that are not part of the lookup.

--- lib/AST/DeclBase.cpp
+++ lib/AST/DeclBase.cpp
@@ -1152,8 +1152,8 @@ void DeclContext::removeDecl(Decl *D) {
   if (isa<NamedDecl>(D)) {
     NamedDecl *ND = cast<NamedDecl>(D);
 
-    // Remove only decls that have a name
-    if (!ND->getDeclName()) return;
+    // Remove only decls that have a name or registered in the lookup.
+    if (!ND->getDeclName() || ND->isHidden()) return;
 
     StoredDeclsMap *Map = D->getDeclContext()->getPrimaryContext()->LookupPtr.getPointer();
     if (!Map) return;
