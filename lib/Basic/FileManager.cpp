3a6d15b296370f13b84718d5f477cd75bdb1ce49
Axel Naumann <Axel.Naumann@cern.ch>
2014-01-07 12:05:19 +0100

    Make sure the inode did not get updated since we read it.

--- lib/Basic/FileManager.cpp
+++ lib/Basic/FileManager.cpp
@@ -273,7 +273,8 @@ const FileEntry *FileManager::getFile(StringRef Filename, bool openFile,
   FileEntry &UFE = UniqueRealFiles[Data.UniqueID];
 
   NamedFileEnt.setValue(&UFE);
-  if (UFE.isValid()) { // Already have an entry with this inode, return it.
+  if (UFE.isValid() && Data.ModTime == UFE.ModTime) {
+    // Already have an entry with this inode, return it.
     // If the stat process opened the file, close it to avoid a FD leak.
     if (F)
       delete F;
