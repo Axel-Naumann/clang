afdc1583de201052109a187acb946212c28da70d
Axel Naumann <Axel.Naumann@cern.ch>
2014-01-07 12:05:19 +0100

    Make sure the inode did not get updated since we read it.

--- lib/Basic/FileManager.cpp
+++ lib/Basic/FileManager.cpp
@@ -280,6 +280,12 @@ const FileEntry *FileManager::getFile(StringRef Filename, bool openFile,
     // multiple names.
     if (DirInfo != UFE.Dir && Data.IsVFSMapped)
       UFE.Dir = DirInfo;
+  }
+  if (UFE.isValid() && Data.ModTime == UFE.ModTime) {
+    // Already have an entry with this inode, return it.
+    // If the stat process opened the file, close it to avoid a FD leak.
+    if (F)
+      delete F;
 
     return &UFE;
   }
