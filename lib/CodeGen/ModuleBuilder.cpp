716a25ab045e1ca957299701eb4d074c497e9c5a
Vassil Vassilev <vvasilev@cern.ch>
2014-02-27 15:50:03 +0100

    Find an easier way of accessing CodeGenModule internals.

--- lib/CodeGen/ModuleBuilder.cpp
+++ lib/CodeGen/ModuleBuilder.cpp
@@ -27,7 +27,7 @@
 #include <memory>
 using namespace clang;
 
-namespace {
+namespace clang {
   class CodeGeneratorImpl : public CodeGenerator {
     DiagnosticsEngine &Diags;
     ASTContext *Ctx;
