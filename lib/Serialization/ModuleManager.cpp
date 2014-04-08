6748f6a30f10aff71fc01f900277dde17fc81dde
Vassil Vassilev <vvasilev@cern.ch>
2014-04-08 18:00:07 +0200

    Adapt the user of invalidateCache to its new signature. Note: this might be change of behavior, because the new implementation doesn't erase the entry from the ADT.

--- lib/Serialization/ModuleManager.cpp
+++ lib/Serialization/ModuleManager.cpp
@@ -198,6 +198,7 @@ void ModuleManager::removeModules(
   for (ModuleIterator victim = first; victim != last; ++victim) {
     Modules.erase((*victim)->File);
 
+    FileMgr.invalidateCache(const_cast<FileEntry*>((*victim)->File));
     if (modMap) {
       StringRef ModuleName = (*victim)->ModuleName;
       if (Module *mod = modMap->findModule(ModuleName)) {
