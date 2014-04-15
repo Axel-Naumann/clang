f4c4c9a603f889d4d98ed70292fd1e94043621f1
Axel Naumann <Axel.Naumann@cern.ch>
2014-04-15 12:11:18 +0200

    Propagate cache flags from LookupFile() to FileManager::getFile().

--- include/clang/Lex/HeaderSearch.h
+++ include/clang/Lex/HeaderSearch.h
@@ -383,7 +383,8 @@ public:
       ArrayRef<std::pair<const FileEntry *, const DirectoryEntry *>> Includers,
       SmallVectorImpl<char> *SearchPath, SmallVectorImpl<char> *RelativePath,
       Module *RequestingModule, ModuleMap::KnownHeader *SuggestedModule,
-      bool SkipCache = false);
+      bool SkipCache = false,
+      bool OpenFile = true, bool CacheFailures = true);
 
   /// \brief Look up a subframework for the specified \#include file.
   ///
@@ -584,7 +585,8 @@ private:
   const FileEntry *
   getFileAndSuggestModule(StringRef FileName, const DirectoryEntry *Dir,
                           bool IsSystemHeaderDir, Module *RequestingModule,
-                          ModuleMap::KnownHeader *SuggestedModule);
+                          ModuleMap::KnownHeader *SuggestedModule,
+                          bool OpenFile = true, bool CacheFailures = true);
 
 public:
   /// \brief Retrieve the module map.
