bcefb6f80ea2fd7f437cba4a46fe9952666e768c
Vassil Vassilev <vvasilev@cern.ch>
2014-06-05 17:36:12 +0200

    Help git do a 'proper' merging and adapt to the semantic changes

--- include/clang/Lex/Preprocessor.h
+++ include/clang/Lex/Preprocessor.h
@@ -759,7 +759,8 @@ public:
   public:
     CleanupAndRestoreCacheRAII(Preprocessor &PP)
       : PP(PP), SavedCachedLexPos(PP.CachedLexPos), 
-        SavedCachedTokens(PP.CachedTokens), SavedStack(PP.IncludeMacroStack),
+        SavedCachedTokens(PP.CachedTokens),
+        SavedStack(std::move(PP.IncludeMacroStack)),
         SavedCurLexer(PP.CurLexer.release()), 
         SavedCurPTHLexer(PP.CurPTHLexer.release()),
         SavedCurPPLexer(PP.CurPPLexer), 
@@ -785,7 +786,7 @@ public:
       //ExitCachingLexMode();
       PP.CachedLexPos = SavedCachedLexPos;
       PP.CachedTokens = SavedCachedTokens;
-      PP.IncludeMacroStack = SavedStack;
+      PP.IncludeMacroStack = std::move(SavedStack);
       PP.CurLexer.reset(SavedCurLexer);
       PP.CurPTHLexer.reset(SavedCurPTHLexer);
       PP.CurPPLexer = SavedCurPPLexer;
